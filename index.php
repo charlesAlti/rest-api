5d68d001ed9f6ff0b6cf75c53f15751033a324aa
Carlos Gonzalez <cgonzalez@altimetrik.com>
2022-01-10 11:43:43 -0500

    Adding service

--- index.php
+++ index.php
@@ -7,18 +7,18 @@ header('Content-type:application/json;charset=utf-8');
 }*/
 
 //Make sure that the content type of the POST request has been set to application/json
-$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
-$contentIsJson = strpos($contentType, "application/json");
+//$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+//$contentIsJson = strpos($contentType, "application/json");
 
 /*if ($contentIsJson === false){
     throw new Exception('Content type must be: application/json');
 }*/
 
 //Receive the RAW post data.
-$content = trim(file_get_contents("php://input"));
+//$content = trim(file_get_contents("php://input"));
 
 //Attempt to decode the incoming RAW post data from JSON.
-$decoded = json_decode($content, true);
+//$decoded = json_decode($content, true);
 
 //$app = strtoupper($decoded['API']);
 //$key = $decoded['APIKEY'];
